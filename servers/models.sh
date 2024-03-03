c462ad91a66c87697fdfd777161b684e2d513806
Joe Huss <detain@interserver.net>
2024-03-03 06:01:11 -0500

    update

--- servers/models.sh
+++ servers/models.sh
@@ -4,7 +4,7 @@ IFS="
 echo "const serverModels = {";
 for i in [A-Za-z]*; do
   echo -n "'$i': [";
-  for m in $(find "$i" -type f|cut -d/ -f2-); do
+  for m in $(find "$i/model.dae" "$i/model" -type f 2>/dev/null|cut -d/ -f2-); do
     echo -n "'$m',";
   done;
   echo "],";
